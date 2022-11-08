4d227a133b0db0db5f7254ba3281a90bc7f3ea2c
Nikita5152 <114350496+Nikita5152@users.noreply.github.com>
2022-11-08 10:56:13 +0300

    Add files via upload

---
+++ Тортики/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тортики
+{
+    internal class Global
+    {
+        public static int Price = 0;
+        public static List<string> Order = new List<string> { };
+    }
+}
